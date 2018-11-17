dee1eeec02c6d8beef4924b28d9892059b3dcd76
Marc Leeman <marc.leeman@gmail.com>
2018-11-17 01:28:21 +0100

    gst: initial type definitions

--- gst/gstrtpsink.h
+++ gst/gstrtpsink.h
@@ -1,3 +1,11 @@
 #ifndef _GST_RTP_SINK_H_
 #define _GST_RTP_SINK_H_
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+#define GST_TYPE_RTP_SINK (gst_rtp_sink_get_type ())
+G_DECLARE_FINAL_TYPE (GstRtpSink, gst_rtp_sink, GST, RTP_SINK, GstBin);
+G_END_DECLS
+
 #endif
--- gst/gstrtpsrc.h
+++ gst/gstrtpsrc.h
@@ -1,3 +1,11 @@
 #ifndef _GST_RTP_SRC_H_
 #define _GST_RTP_SRC_H_
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+#define GST_TYPE_RTP_SRC (gst_rtp_src_get_type ())
+G_DECLARE_FINAL_TYPE (GstRtpSrc, gst_rtp_src, GST, RTP_SRC, GstBin);
+G_END_DECLS
+
 #endif
