f57c64cce927b36b1c978c79f664ad68f9754538
Marc Leeman <marc.leeman@gmail.com>
2018-11-18 21:46:54 +0100

    gst: set params from uri query

---
+++ gst_object_set_properties_from_uri_query.h
@@ -0,0 +1,10 @@
+#ifndef __GST_OBJECT_SET_PROPERTIES_FROM_URI_QUERY_H__
+#define __GST_OBJECT_SET_PROPERTIES_FROM_URI_QUERY_H__
+
+#include <gst/gst.h>
+
+void
+gst_object_set_properties_from_uri_query (GObject * obj, const GstUri * uri);
+
+#endif
+
