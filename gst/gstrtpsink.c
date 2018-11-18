2a164df0fb971cde6b895dc1605d3857aadc3767
Marc Leeman <marc.leeman@gmail.com>
2018-11-18 02:47:32 +0100

    rtpsink: add uri property/handler

--- gst/gstrtpsink.c
+++ gst/gstrtpsink.c
@@ -7,17 +7,28 @@
 GST_DEBUG_CATEGORY_STATIC (rtp_sink_debug);
 #define GST_CAT_DEFAULT rtp_sink_debug
 
+#define DEFAULT_PROP_URI              "rtp://0.0.0.0:5004"
+
 struct _GstRtpSink
 {
   GstBin parent_instance;
+
+  /* Properties */
+  GstUri *uri;
+
   GMutex lock;
 };
 
 enum
 {
   PROP_0,
+
+  PROP_URI,
+
   PROP_LAST
 };
+
+static void gst_rtp_sink_uri_handler_init (gpointer g_iface, gpointer iface_data); 
 
 #define gst_rtp_sink_parent_class parent_class
 G_DEFINE_TYPE_WITH_CODE (GstRtpSink, gst_rtp_sink, GST_TYPE_BIN,
@@ -29,7 +40,12 @@ gst_rtp_sink_set_property (GObject * object, guint prop_id,
     const GValue * value, GParamSpec * pspec)
 {
   GstRtpSink *self = GST_RTP_SINK (object);
+
   switch (prop_id) {
+    case PROP_URI:
+      if (self->uri) gst_uri_unref (self->uri);
+      self->uri = gst_uri_from_string (g_value_get_string (value));
+      break;
         default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -41,6 +57,13 @@ gst_rtp_sink_get_property (GObject * object, guint prop_id,
     GValue * value, GParamSpec * pspec)
 {
   GstRtpSink *self = GST_RTP_SINK (object);
+
+  switch (prop_id) {
+    case PROP_URI:
+      if (self->uri)
+        g_value_take_string (value, gst_uri_to_string (self->uri));
+      else
+        g_value_set_string (value, NULL);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -51,6 +74,10 @@ gst_rtp_sink_get_property (GObject * object, guint prop_id,
 static void
 gst_rtp_sink_finalize (GObject * gobject)
 {
+  GstRtpSink *self = GST_RTP_SINK (gobject);
+
+  if (self->uri) gst_uri_unref (self->uri);
+
   G_OBJECT_CLASS (parent_class)->finalize (gobject);
 }
 
@@ -64,6 +91,10 @@ gst_rtp_sink_class_init (GstRtpSinkClass * klass)
   oclass->get_property = gst_rtp_sink_get_property;
   oclass->finalize = gst_rtp_sink_finalize;
 
+  g_object_class_install_property (oclass, PROP_URI,
+      g_param_spec_string ("uri", "URI", "URI to send data on",
+          DEFAULT_PROP_URI, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
   GST_DEBUG_CATEGORY_INIT (rtp_sink_debug,
       "rtpsink", 0, "GStreamer RTP sink");
 }
@@ -71,5 +102,52 @@ gst_rtp_sink_class_init (GstRtpSinkClass * klass)
 static void
 gst_rtp_sink_init (GstRtpSink * self)
 {
+  self->uri = gst_uri_from_string (DEFAULT_PROP_URI);
 
+  GST_OBJECT_FLAG_SET (GST_OBJECT (self), GST_ELEMENT_FLAG_SINK);
 }
+
+static guint
+gst_rtp_sink_uri_get_type (GType type)
+{
+  return GST_URI_SINK;
+}
+
+static const gchar *const *
+gst_rtp_sink_uri_get_protocols (GType type)
+{
+  static const gchar *protocols[] = { (char *) "rtp", NULL };
+
+  return protocols;
+}
+
+static gchar *
+gst_rtp_sink_uri_get_uri (GstURIHandler * handler)
+{
+  GstRtpSink *self = (GstRtpSink *) handler;
+
+  return gst_uri_to_string (self->uri);
+}
+
+static gboolean
+gst_rtp_sink_uri_set_uri (GstURIHandler * handler, const gchar * uri, GError ** error)
+{
+  GstRtpSink *self = (GstRtpSink *) handler;
+
+  g_object_set (G_OBJECT (self), "uri", uri, NULL);
+
+  return TRUE;
+}
+
+static void
+gst_rtp_sink_uri_handler_init (gpointer g_iface, gpointer iface_data)
+{
+  GstURIHandlerInterface *iface = (GstURIHandlerInterface *) g_iface;
+
+  iface->get_type = gst_rtp_sink_uri_get_type;
+  iface->get_protocols = gst_rtp_sink_uri_get_protocols;
+  iface->get_uri = gst_rtp_sink_uri_get_uri;
+  iface->set_uri = gst_rtp_sink_uri_set_uri;
+}
+
+
