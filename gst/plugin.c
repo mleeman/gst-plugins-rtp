7e8659ebfbffdc6737b0c572c2b2643e104ae6b7
Marc Leeman <marc.leeman@gmail.com>
2018-11-22 20:14:18 +0100

    plugin: gst-indent

--- gst/plugin.c
+++ gst/plugin.c
@@ -13,10 +13,10 @@ plugin_init (GstPlugin * plugin)
   gboolean ret = FALSE;
 
   ret |= gst_element_register (plugin, "rtpsrc",
-            GST_RANK_PRIMARY + 1, GST_TYPE_RTP_SRC);
+      GST_RANK_PRIMARY + 1, GST_TYPE_RTP_SRC);
 
   ret |= gst_element_register (plugin, "rtpsink",
-            GST_RANK_PRIMARY + 1, GST_TYPE_RTP_SINK);
+      GST_RANK_PRIMARY + 1, GST_TYPE_RTP_SINK);
 
   return ret;
 }
@@ -26,4 +26,3 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     nrtp,
     "GStreamer RTP Plugins",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
-
