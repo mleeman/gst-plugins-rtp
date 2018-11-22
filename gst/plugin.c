5e964fc63812c9c6a838fea51d49c1051e03a124
Marc Leeman <marc.leeman@gmail.com>
2018-11-22 20:06:18 +0100

    rtp: rename library file name to nrtp
    the rtp library is part of the gst-plugins-good, to avoid a conflict
    rename to nrtp

--- gst/plugin.c
+++ gst/plugin.c
@@ -23,7 +23,7 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    rtp,
+    nrtp,
     "GStreamer RTP Plugins",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
 
